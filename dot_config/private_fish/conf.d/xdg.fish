864c5c487ead3bbd806188c7be5590f3af383884
Saulo Lordão Andrade Barros <saulo.lordao@gmail.com>
2020-01-31 23:08:05 -0500

    (fish) Fixed XDG variables.

--- dot_config/private_fish/conf.d/xdg.fish
+++ dot_config/private_fish/conf.d/xdg.fish
@@ -1,2 +1,2 @@
-set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME ~/".config"
-set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME ~/".cache"
+set -q XDG_CONFIG_HOME || set -gx XDG_CONFIG_HOME $HOME/.config
+set -q XDG_CACHE_HOME || set -gx XDG_CACHE_HOME $HOME/.cache
