088e85a90c7c85ebe9f411ee71f4975715f55b7c
Saulo Lordão Andrade Barros <saulo@physiotec.org>
2020-02-03 10:04:04 -0500

    (fish) Added snap data dir to XDG_DATA_DIRS.

--- dot_config/private_fish/conf.d/xdg.fish
+++ dot_config/private_fish/conf.d/xdg.fish
@@ -1,2 +1,11 @@
 set -q XDG_CONFIG_HOME || set -gx XDG_CONFIG_HOME $HOME/.config
 set -q XDG_CACHE_HOME || set -gx XDG_CACHE_HOME $HOME/.cache
+
+# XDG_DATA_DIRS additions
+set -l dirs_to_add /var/lib/snapd/desktop
+set -l data_dirs (string split ':' $XDG_DATA_DIRS)
+for dir in dirs_to_add
+    if test -d $dir && not contains $dir $data_dirs
+        set -gx XDG_DATA_DIRS "$XDG_DATA_DIRS:/var/lib/snapd/desktop"
+    end
+end
