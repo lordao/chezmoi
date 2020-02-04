7c213b11b6a73293c9ff3c33facd2d6bef57b6e8
Saulo Lordão Andrade Barros <saulo@physiotec.org>
2020-02-04 11:41:47 -0500

    (fish) Set variable as local variable, so it won't pollute env.

--- dot_config/private_fish/conf.d/env.fish
+++ dot_config/private_fish/conf.d/env.fish
@@ -1,8 +1,8 @@
-set myPaths ~/.local/bin ~/.npm-global/bin ~/Android/Sdk/tools
-for path in $myPaths
+set -l my_paths ~/.local/bin ~/.npm-global/bin ~/Android/Sdk/tools
+for path in $my_paths
     if not contains $path $PATH
         set -gx PATH $path $PATH
     end
 end
 set DOTNET_CLI_TELEMETRY_OPTOUT 1
-set EDITOR "nvim"
+set EDITOR nvim
